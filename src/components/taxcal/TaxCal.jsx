e324824da55d528607f0c9086d13c04e30112ad1
G-Dharani-raj <gdharaniraj0@gmail.com>
2023-06-21 20:09:23 +0530

    fields

---
+++ src/components/taxcal/TaxCal.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Individual from "./payers/Individual";
+
+const TaxCal = () => {
+	return (
+		<div>
+			TaxCal
+			<Individual />
+		</div>
+	);
+};
+
+export default TaxCal;
